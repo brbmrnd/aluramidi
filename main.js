e2302a0c8cccf22a157b3e24fca5ebd121db44de
bárbara miranda <barbaramntmiranda@gmail.com>
2024-06-07 20:07:03 -0300

    add functions

--- main.js
+++ main.js
@@ -1,8 +1,22 @@
 const listaAudios = document.querySelectorAll('.audio');
 const listaTeclas = document.querySelectorAll('.tecla');
 
-for (let i = 0; i < listaTeclas.length; i++){
-    listaTeclas[i].onclick = function(){
+for (let i = 0; i < listaTeclas.length; i++) {
+    const tecla = listaTeclas[i];
+
+    //ativa a tecla com o click
+    tecla.onclick = function () {
         listaAudios[i].play();
+    }
+    //ativa a tecla com o enter/barra de espaço
+    tecla.onkeydown = function (eventKey) {
+        if (eventKey.code === 'Enter' || eventKey.code === 'Space') {
+            tecla.classList.add('ativa');
+        }
+    }
+
+    //remove o estado ativo da tecla ao soltar os botões
+    tecla.onkeyup = function () {
+        tecla.classList.remove('ativa');
     }
 }
